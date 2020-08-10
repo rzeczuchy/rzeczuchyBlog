be9c23cca87693b6bb0981b1351ab56230e920ae
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2020-08-10 21:42:25 +0200

    setting up views and index template

--- rzeczuchyBlog/blog/views.py
+++ rzeczuchyBlog/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def index(request):
+   post_list = Post.objects.all()
+   context = {'post_list': post_list}
+   return render(request, 'blog/index.html', context)
