991d7d8213ce2efcc13f98b1da16b918ab09f78e
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2020-08-10 21:19:56 +0200

    adding Post to models, adding migration

--- rzeczuchyBlog/blog/models.py
+++ rzeczuchyBlog/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    published_time = models.DateTimeField('published at')
+    author = models.CharField(max_length=50)
+    main_content = models.CharField(max_length=2500)
