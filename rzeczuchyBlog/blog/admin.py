bfdffc0c228b752ee1169d2b7b57622fe8abe9f4
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2020-08-10 22:29:15 +0200

    updating the admin area

--- rzeczuchyBlog/blog/admin.py
+++ rzeczuchyBlog/blog/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Post
 
+admin.site.site_header = "rzeczuchyBlog admin"
+admin.site.site_title = "rzeczuchyBlog admin"
+
 # Register your models here.
 admin.site.register(Post)
