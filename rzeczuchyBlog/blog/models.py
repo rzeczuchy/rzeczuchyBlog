ac5768e5474cf880e9f5e40b1949d52d11ecf03e
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2020-08-12 21:08:56 +0200

    adding the ckeditor as rich-text editor

--- rzeczuchyBlog/blog/models.py
+++ rzeczuchyBlog/blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
     published_time = models.DateTimeField('published at')
     author = models.CharField(max_length=50)
-    main_content = models.TextField(max_length=2500)
+    main_content = RichTextField(blank=True, null=True)
+    #main_content = models.TextField(max_length=2500)
 
     def __str__(self):
         return self.title
