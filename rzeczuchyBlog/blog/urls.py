f4d495c39c7ccddd862676f7a509debea378700b
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2020-08-12 10:24:11 +0200

    adding post detail view, extracting shared html

--- rzeczuchyBlog/blog/urls.py
+++ rzeczuchyBlog/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import PostDetail
  
 app_name = 'blog'
 urlpatterns = [
-   path('', views.index, name="index")
+   path('', views.index, name="index"),
+   path('post/<int:pk>', PostDetail.as_view(), name='post-detail'),
 ]
--- rzeczuchyBlog/blog/views.py
+++ rzeczuchyBlog/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 from .models import Post
 
 # Create your views here.
@@ -6,3 +7,7 @@ def index(request):
    post_list = Post.objects.order_by('-published_time')
    context = {'post_list': post_list}
    return render(request, 'blog/index.html', context)
+
+class PostDetail(DetailView):
+   model = Post
+   template_name = 'blog/post_detail.html'
