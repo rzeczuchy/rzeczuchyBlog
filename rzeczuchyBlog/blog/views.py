85f45c3896d8b1eefe2715dde9fcbb69dcaca64e
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2020-08-13 08:02:28 +0200

    adding pagination to index, mild cleanup

--- rzeczuchyBlog/blog/views.py
+++ rzeczuchyBlog/blog/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
+from django.core.paginator import Paginator
 from .models import Post
 
 # Create your views here.
 def index(request):
    post_list = Post.objects.order_by('-published_time')
-   context = {'post_list': post_list}
-   return render(request, 'blog/index.html', context)
+   paginator = Paginator(post_list, 3)
+   page_number = request.GET.get('page')
+   page_obj = paginator.get_page(page_number)
+   return render(request, 'blog/index.html', {'page_obj': page_obj})
 
 class PostDetail(DetailView):
    model = Post
